ef27a795de135739e2d21ea1f879f56ee1d51d36
Fedcomp <aglergen@gmail.com>
2020-03-05 20:45:18 +0300

    webpack-dev-server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development", // "production" | "development" | "none"
@@ -6,5 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
-  devtool: "inline-source-map"
+  devtool: "inline-source-map",
+  plugins: [
+    new CopyPlugin([
+      { from: 'src/index.html' },
+    ]),
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    host: '0.0.0.0',
+    port: 4000
+  }
 }
