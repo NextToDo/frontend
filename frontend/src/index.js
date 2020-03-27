c8fafafcc757bb781738bb088463379737d0d01b
Fedcomp <aglergen@gmail.com>
2020-03-28 02:21:18 +0300

    preact: init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!')
+import { h, render, Component } from 'preact';
+
+class App extends Component {
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
+}
+
+render(<App />, document.body);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -13,6 +13,26 @@ module.exports = {
       { from: 'src/index.html' },
     ]),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
+  resolve: { 
+    alias: { 
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      // Must be below test-utils
+      "react-dom": "preact/compat",
+    },
+  },
   devServer: {
     // contentBase: path.join(__dirname, 'dist'),
     host: '0.0.0.0',
