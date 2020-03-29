1358017aab6758a0dcb130c3a1ca24f3844a97a8
Fedcomp <aglergen@gmail.com>
2020-03-29 17:46:49 +0300

    Remove babel entirely

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -24,14 +24,6 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: { presets: ['@babel/preset-env'] }
-        }
-      },
-      {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         loaders: ['ts-loader']
